670e033d57927ccbc44a0266acd3b4b0709a7164
David Melamed <dvdmelamed@gmail.com>
2024-09-05 09:57:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import sys
+
+def get_current_weather():
+  return "Good weather"
+
 if __name__ == "__main__":
-  print("hello this is a test"
+  print("hello this is a test")
+  print(get_current_weather())
+  sys.exit(2)
